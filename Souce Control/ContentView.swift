afc1c87470087bab73174d1b5ccbc8b342251b1a
Craig Kabrhel <craig.kabrhel@gmail.com>
2022-05-17 13:32:55 -0400

    Initial Commit

---
+++ Souce Control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Souce Control
+//
+//  Created by Craig Kabrhel on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Souce Control/Souce_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Souce_ControlApp.swift
+//  Souce Control
+//
+//  Created by Craig Kabrhel on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Souce_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
